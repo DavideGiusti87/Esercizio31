f89d6751645d9ab7d03febadbeed87b05bb92676
DavideGiusti87 <99611360+DavideGiusti87@users.noreply.github.com>
2022-05-23 17:59:27 +0200

    Esercizio31.1: String Handling 04

--- src/Main.java
+++ src/Main.java
@@ -15,15 +15,18 @@ public class Main {
     public static void main(String[] args) {
         String veryLongString = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-        veryLongString.replace("um","HUM");
+        veryLongString = veryLongString.replace("um", "HUM");
 
-        String sub1= veryLongString.substring(0,veryLongString.indexOf("aliquip"));
-        String sub2= veryLongString.substring(veryLongString.indexOf("aliquip"));
+        String sub1 = veryLongString.substring(0, veryLongString.indexOf("aliquip"));
+        String sub2 = veryLongString.substring(veryLongString.indexOf("aliquip"));
 
-        System.out.println(sub1.trim());
-        System.out.println(sub2.trim());
+        sub1 = sub1.trim();
+        sub2 = sub2.trim();
 
-        System.out.println(sub1.indexOf("est"));
-        System.out.println(sub2.indexOf("est"));
+        System.out.println(sub1);
+        System.out.println(sub2);
+
+        System.out.println("One of the two strings contains \"est\": "+(sub1.contains("est") ^ sub2.contains("est")));
+
     }
 }
